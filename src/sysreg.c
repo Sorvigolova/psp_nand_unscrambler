9b1b0ccbbe1980c024c4160279f2c465491e6a4a
Sorvigolova <justhomeuser@gmail.com>
2024-07-05 15:24:33 +0400

    Update sysreg.c
    Fixed sysreg defaults

--- src/sysreg.c
+++ src/sysreg.c
@@ -3,9 +3,9 @@
 
 #include "sysreg.h"
 
-unsigned int g_tachyon_version = 0x820000;
+unsigned int g_tachyon_version = 0;
 //unsigned long long g_fuseid = 0xCA79F317271E;
-unsigned long long g_fuseid = 0x353C8C0AAF90;
+unsigned long long g_fuseid = 0;
 
 unsigned long long pspSysregGetFuseId()
 {
